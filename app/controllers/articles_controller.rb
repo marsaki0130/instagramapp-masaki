29bfe11ff3af05f4dd459c25c466b22283d8c031
正木啓介 <kettan0130@gmail.com>
2021-12-10 08:40:47 +0900

    faker導入

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!
 
     def index
       @articles = Article.all
@@ -9,6 +10,17 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        @articles = current_user.build()
+        @article = current_user.articles.build(article_params)
+        if @article.save
+          redirect_to article_path(@article), notice: '保存できました'
+        else
+          flash.now[:error] = '失敗しました'
+          render.new
+        end
+    end
+
+    private
+    def article_params
+        params.require(:article).permit(:title, :content, :eyecatch)
     end
 end
