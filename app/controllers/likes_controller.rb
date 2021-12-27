77a09496998f97712e666a324c0da765f40df204
正木啓介 <kettan0130@gmail.com>
2021-12-27 16:30:49 +0900

    「いいね」微調整

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,4 +6,11 @@ class LikesController < ApplicatioonController
       article.likes.create!(user_id: current_user.id) #article_idまでは入った→その後にuser_idを入れてあげる
       redirect_to article_path(article) #「いいね」したらarticleの記事に戻る
     end
+
+    def destroy
+      article = Article.find(params[:article_id])
+      like = article.likes.find_by!(user_id: current_user.id)
+      like.destroy!
+      redirect_to article_path(article)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,10 @@ class User < ApplicationRecord
     profile || build_profile
   end
 
+  def has_liked?(article)
+    likes.exists?(article_id: article.id)
+  end
+
   def avatar_image
     if profile&.avatar&.attached?
       profile.avatar
