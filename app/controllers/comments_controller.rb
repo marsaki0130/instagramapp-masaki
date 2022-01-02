7050b8982868934a2d9419c9c7d953fc9692b9ae
正木啓介 <kettan0130@gmail.com>
2022-01-02 11:21:11 +0900

    create-actionとvalidate追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,21 @@ class CommentsController < ApplicationController
     article = Article.find(params[:article_id]) #/articles/:article_id/comment/new
     @comment = article.comments.build
   end
+
+  def create
+    article = Article.find(params[:article_id])
+    @comment = article.comments.build(comment_params)
+    if @comment.save
+      redirect_to article_path(article), notice:'コメントを追加'
+    else
+      flash.now[:error] = '失敗しました'
+      render :new
+    end
+
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
